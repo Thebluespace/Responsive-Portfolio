52aa6277e6c7c31b20321b676b9ccea218b772bf
Thomas Greene <tecgreene93@gmail.com>
2018-07-02 17:42:57 -0400

    intial files copied

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+function indexButton() {
+    window.location.href="index.html";
+}
+function contactButton() {
+    window.location.href="contact.html";
+}
+function portfolioButton() {
+    window.location.href="portfolio.html";
+}
+
+function pbutton1() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton2() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton3() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton4() {
+    window.location.href="https://www.google.com/";
+}
+function pbutton5() {
+    window.location.href="https://www.google.com/";
+}
